95be19619837cb824e0bf95745c46535e29b1676
kenny_onne <kennyonne5@gmail.com>
2024-11-21 17:11:48 +0100

    j'ai changer le bacground etcorriger 2 3 erreurs

--- index.js
+++ index.js
@@ -1,11 +1,48 @@
-let containerEts = document.getElementsByClassName("intitule");
+ // Votre code GSAP ici
+ let element = document.getElementsByClassName("ets")
+ // some example properties.
+ gsap.to(element, {
+    
+    fontSize : 140 ,
+    position : "relative" ,
+    x:14 ,
+    y:-40 ,
+    yoyo : 1,
+    duration : 1.4
+    
+ });
+ let body = document.body
 
-// Convertir la collection en tableau pour itérer
-Array.from(containerEts).forEach((element) => {
-  gsap.to(element, {
-    top: -45,
-    repeat: -1, // Animation infinie
-    yoyo: true, // Revenir à la position d'origine
-    duration: 1 // Durée de l'animation
-  });
-});
+ gsap.set(body ,{ 
+     display : 'flex',
+     justifyContent: "center" ,
+     alignItems : "center"
+ })
+
+
+//  le but de de faire a ce que tous le contenu de la page s'affiche sans scroll
+
+
+
+document.addEventListener("DOMContentLoaded" , function(){
+    const links = document.getElementById("nav-link");
+    const sections = document.querySelectorAll(".section");
+
+    links.forEach(link => {
+        link.addEventListener("click" , function(e){
+            e.preventDefault();
+
+            // masquer les autres section
+            sections.forEach(section=> {
+                section.classList.remove("show");
+            })
+
+            // obtenir la cible a afficher  (la section selectionner)
+            const targetId = this.getAttribute("data-target");
+            const targetSection = document.getElementById(targetId);
+
+            // affichage de la section
+            targetSection.classList.add("show");
+        })
+    });
+})
